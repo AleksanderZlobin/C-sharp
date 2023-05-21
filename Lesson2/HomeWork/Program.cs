4c70dd6c82d285aea30ed5cd10cb2d5ebaa73549
Aleksander_Zlobin <lancer111187@mail.ru>
2023-05-21 20:18:41 +0700

    add Lesson2 Homework comment at task # 8

--- Lesson2/HomeWork/Program.cs
+++ Lesson2/HomeWork/Program.cs
@@ -47,8 +47,8 @@
 // Напишите программу, которая на вход принимает число (N), а на выходе 
 // показывает все четные числа от 1 до N.
 
-Console.Clear();
-Console.Write("Введите число: ");
-int N = int.Parse(Console.ReadLine()!);
-for (int i = 2; i <= N; i+=2)
-    Console.Write($"{i} ");
+// Console.Clear();
+// Console.Write("Введите число: ");
+// int N = int.Parse(Console.ReadLine()!);
+// for (int i = 2; i <= N; i+=2)
+//     Console.Write($"{i} ");
