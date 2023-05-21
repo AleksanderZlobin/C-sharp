83a5f91c9d5fc6af8f8a09de1acc23625504b12e
Aleksander_Zlobin <lancer111187@mail.ru>
2023-05-21 20:34:29 +0700

    add Lesson4 Theory

---
+++ Lesson4/Theory/Program.cs
@@ -0,0 +1,11 @@
+﻿// Console.Clear();
+// Console.Write("Введите число: ");
+// int n = int.Parse(Console.ReadLine()!);
+// Console.WriteLine(n > 99 && n < 1000); // n = 1001
+// //                  1     +     0   = 0 (False)
+// // || - или (дизъюнкция) - сложение (+) если хотя бы один True то True
+// // && - b (конъюнкция) - умножение (*) если хотя бы один False то False
+// if (n % 3 == 0 && n % 7 == 0) // Проверяет является ли число одновременно кратным 3 и 7
+//     Console.WriteLine("yes");
+// else
+//     Console.WriteLine("no");
