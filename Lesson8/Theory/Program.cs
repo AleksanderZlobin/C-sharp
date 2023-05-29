093ad0262562cfaad5955d015fbde6bb147f78ae
AleksanderZlobin <lancer111187@mail.ru>
2023-05-29 20:51:19 +0700

    add Lesson8 Theory

---
+++ Lesson8/Theory/Program.cs
@@ -0,0 +1,8 @@
+﻿// array
+// Console.Clear();
+// Console.Write("Введите кол-во элементов массива: ");
+// int n = int.Parse(Console.ReadLine()!);
+// int[] array = new int[n]; // Массив по умолчанию(числовой) заполняется нулями
+// for (int i = 0; i < array.Length; i++)
+//     array[i] = new Random().Next(1, 11); // [1; 10]
+// Console.WriteLine($"Результат: [{string.Join(", ", array)}]");
