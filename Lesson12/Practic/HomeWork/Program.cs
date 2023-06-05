5eaed7ec6e3edfac76efc491f1f0a597343c33ab
AleksanderZlobin <lancer111187@mail.ru>
2023-06-05 22:47:10 +0700

    add Lesson12 Practic HomeWork

---
+++ Lesson12/Practic/HomeWork/Program.cs
@@ -0,0 +1,45 @@
+﻿// Task # 41
+// Пользователь вводит с клавиатуры M чисел. Посчитайте,
+// сколько чисел больше 0 ввёл пользователь.
+
+// Console.Clear();
+// Console.Write("Введите количество чисел: ");
+// int m = int.Parse(Console.ReadLine()!);
+// int count = 0;
+// for (int i = 0; i < m; i++)
+// {
+//     Console.WriteLine($"Введите {i + 1} число: ");
+//     int n = int.Parse(Console.ReadLine()!);
+//     if (n > 0) count++;
+// }
+// Console.WriteLine($"Количество чисел больше нуля равно {count}");
+
+
+// Task # 43
+// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения 
+// b1, k1, b2 и k2 задаются пользователем.
+
+// Console.Clear();
+// Console.WriteLine(" Введите значение k1: ");
+// double k1 = double.Parse(Console.ReadLine()!);
+// Console.WriteLine(" Введите значение b1: ");
+// double b1 = double.Parse(Console.ReadLine()!);
+// Console.WriteLine(" Введите значение k2: ");
+// double k2 = double.Parse(Console.ReadLine()!);
+// Console.WriteLine(" Введите значение b2: ");
+// double b2 = double.Parse(Console.ReadLine()!);
+// if (k1 == k2 && b1 == b2) Console.WriteLine("Прямые совпадают");
+// else if (k1 == k2) Console.WriteLine("Прямые не пересекаются");
+// else Console.WriteLine($"Координаты точки пересечения: x = {(b2 - b1) / (k1 - k2)}, y = {(k1 * (b2 - b1)) / (k1 - k2) + b1}");
+
+
+
+// Task # 1 Additional 
+// Дана строка, состоящая из N попарно различных символов. Требуется вывести все перестановки символов данной строки.
+
+
+
+
+// Task # 2 Additional
+// По целочисленным координатам вершин треугольника (x1,y1), (x2,y2) и (x3,y3) требуется вычислить его площадь.
