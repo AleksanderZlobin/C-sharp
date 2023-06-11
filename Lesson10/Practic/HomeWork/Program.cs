e05c41b27306664a1e27fa4f9d95851dc2ce55e4
AleksanderZlobin <lancer111187@mail.ru>
2023-06-11 09:42:02 +0700

    add Lesson10 Practic HomeWork Additional Task

--- Lesson10/Practic/HomeWork/Program.cs
+++ Lesson10/Practic/HomeWork/Program.cs
@@ -97,10 +97,39 @@
 
 
 
-// Task # 1 Additional
+// Task # 1 Additional Статистика.
 // Вася не любит английский язык, но каждый раз старается получить хотя бы четверку за четверть, 
 // чтобы оставаться ударником. В текущей четверти Вася заметил следующую закономерность: 
 // по нечетным дням месяца он получал тройки, а по четным – четверки. Так же он помнит, 
 // в какие дни он получал эти оценки. Поэтому он выписал на бумажке все эти дни для того, 
 // чтобы оценить, сколько у него троек и сколько четверок. Помогите Васе это сделать, расположив 
 // четные и нечетные числа в разных строчках. Вася может рассчитывать на оценку 4, если четверок не меньше, чем троек.
+
+
+// internal class Program
+// {
+//     private static void Main(string[] args)
+//     {
+//         int n = int.Parse(Console.ReadLine()!), count = 0;
+//         int[] size = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+//         foreach (int x in size)
+//         {
+//             if (x % 2 == 1)
+//             {
+//                 Console.Write($"{x} ");
+//                 count++;
+//             }
+//         }
+//         Console.WriteLine();
+//         foreach (int x in size)
+//         {
+//             if (x % 2 == 0)
+//                 Console.Write($"{x} ");
+//         }
+//         Console.WriteLine();
+//         if (count <= n - count)
+//             Console.WriteLine("YES");
+//         else
+//             Console.WriteLine("NO");
+//     }
+// }
