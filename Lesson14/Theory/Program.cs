e3a65f1a8cb81f5b427ee16c8f77df93eee0dc2c
AleksanderZlobin <lancer111187@mail.ru>
2023-06-11 09:37:21 +0700

    add Lesson14 Theory

---
+++ Lesson14/Theory/Program.cs
@@ -0,0 +1,23 @@
+﻿// Генерация случайных дробных чисел.
+
+
+// void InputArray(double[] array)
+// {
+//     int end = 30, begin = 10;
+//     for (int i = 0; i < array.Length; i++)
+//         array[i] = Math.Round(new Random().NextDouble() * (end - begin) + begin, 2);
+// }
+
+// Console.Clear();
+// Console.Write("Введите кол-во элементов массива: ");
+// int n = int.Parse(Console.ReadLine()!);
+// double[] array = new double[n];
+// InputArray(array);
+// Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+
+
+// Ввод нескольких чисел в одну строку.
+
+
+// int[] size = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+// Console.WriteLine(size[0] + size[1]);
